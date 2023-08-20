9e90aecf139860300c3e187ddbde3dd11d519839
Erorr <138451652+Debug100@users.noreply.github.com>
2023-08-21 01:16:42 +0300

    lessons-1/9

---
+++ main.js
@@ -0,0 +1,38 @@
+/*
+Output to Screen
+---window.alert()
+---document.write()
+---console.log()
+
+--syntax
+*/
+
+// window.alert("Hello from js file");
+// show up a message diractly after open the page |not used alot becosue make every thing below it stopped
+
+document.write("<h1>bye</h1>");
+// you can write anything text or element but not agood way
+
+console.log("Hello From js file");
+// print your message in console
+
+/*
+--Console Methoeds
+---log
+---error
+---table
+
+--Web API
+
+--Styling Console
+---Directive %c
+*/
+
+console.log("Log");
+console.error("Error");
+console.table(["osama" , "Ahmed", "Sayed"]); 
+console.log("Hello from %cjs %cfile", "color:red; font-size:40px" , "color:blue; font-size:40px") 
+
+/*
+--ES6
+*/
